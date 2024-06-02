a75cb1019a6c3bba8ae83a66622db486b24c7091
Georgi Bozhinov <ge.bozhinov@gmail.com>
2024-06-02 15:19:34 +0300

    fix ports

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
-    await page.goto('http://localhost:8888/Add-Grade');
+    await page.goto('http://localhost:8090/Add-Grade');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
